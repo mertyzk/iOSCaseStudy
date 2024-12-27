cac854868b1c6020bf206a83fa6483f60ab0e59a
mertyzk <meyuzak@gmail.com>
2024-12-27 22:36:16 +0300

    UI Helper added

---
+++ E-Market/E-Market/Utilities/Helper/UIHelper.swift
@@ -0,0 +1,26 @@
+//
+//  UIHelper.swift
+//  E-Market
+//
+//  Created by Macbook Air on 27.12.2024.
+//
+
+import UIKit
+
+enum UIHelper {
+    
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width                       = view.bounds.width
+        let padding: CGFloat            = 12
+        let minimumItemSpacing: CGFloat = 10
+        let avaibleWidth                = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth                   = avaibleWidth / 3
+        
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+}
