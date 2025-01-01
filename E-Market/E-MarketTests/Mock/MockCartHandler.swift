7e17b3dffc6d9562949cde86453d641dd418809a
mertyzk <meyuzak@gmail.com>
2025-01-01 13:21:21 +0300

    DetailVM tests

--- E-Market/E-MarketTests/Mock/MockCartHandler.swift
+++ E-Market/E-MarketTests/Mock/MockCartHandler.swift
@@ -8,40 +8,66 @@
 import Foundation
 @testable import E_Market
 
+
 class MockCartHandler: CartHandler {
     var mockCartItems: [Product] = []
+    var mockError: DBErrors?
 
     func fetchProductsFromLocalDB(completion: @escaping (Result<[Product], DBErrors>) -> Void) {
-        completion(.success(mockCartItems))
+        if let error = mockError {
+            completion(.failure(error))
+        } else {
+            completion(.success(mockCartItems))
+        }
     }
 
     func addToLocalDB(product: Product, completion: @escaping (Result<Void, DBErrors>) -> Void) {
-        mockCartItems.append(product)
-        completion(.success(()))
+        if let error = mockError {
+            completion(.failure(error))
+        } else {
+            mockCartItems.append(product)
+            completion(.success(()))
+        }
     }
 
     func deleteFromLocalDB(product: Product, completion: @escaping (Result<Void, DBErrors>) -> Void) {
-        mockCartItems.removeAll { $0.id == product.id }
-        completion(.success(()))
+        if let error = mockError {
+            completion(.failure(error))
+        } else {
+            mockCartItems.removeAll { $0.id == product.id }
+            completion(.success(()))
+        }
     }
 
     func updateProductFromLocalDB(product: Product, completion: @escaping (Result<Void, DBErrors>) -> Void) {
-        if let index = mockCartItems.firstIndex(where: { $0.id == product.id }) {
-            mockCartItems[index] = product
+        if let error = mockError {
+            completion(.failure(error))
+        } else {
+            if let index = mockCartItems.firstIndex(where: { $0.id == product.id }) {
+                mockCartItems[index] = product
+            }
+            completion(.success(()))
         }
-        completion(.success(()))
     }
 
     func removeFromCartFromLocalDB(product: Product, completion: @escaping (Result<Void, DBErrors>) -> Void) {
-        if let index = mockCartItems.firstIndex(where: { $0.id == product.id }) {
-            mockCartItems[index].quantity = nil
-            mockCartItems.remove(at: index)
+        if let error = mockError {
+            completion(.failure(error))
+        } else {
+            if let index = mockCartItems.firstIndex(where: { $0.id == product.id }) {
+                mockCartItems[index].quantity = nil
+                mockCartItems.remove(at: index)
+            }
+            completion(.success(()))
         }
-        completion(.success(()))
     }
 
     func clearCartFromLocalDB(completion: @escaping (Result<Void, DBErrors>) -> Void) {
-        mockCartItems.removeAll()
-        completion(.success(()))
+        if let error = mockError {
+            completion(.failure(error))
+        } else {
+            mockCartItems.removeAll()
+            completion(.success(()))
+        }
     }
 }
--- E-Market/E-MarketTests/Mock/MockNetworkManager.swift
+++ E-Market/E-MarketTests/Mock/MockNetworkManager.swift
@@ -5,15 +5,17 @@
 //  Created by Macbook Air on 1.01.2025.
 //
 
-import Foundation
+import UIKit
 @testable import E_Market
 
 class MockNetworkManager: NetworkManagerProtocol {
+    var mockImage: UIImage?
+    var mockError: NetworkErrors?
     var mockResponses: [String: Any] = [:]
-
+    
     func request<T: BaseRequest>(_ request: T, completion: @escaping (Result<T.ResponseType, NetworkErrors>) -> Void) {
         let requestKey = String(describing: T.self)
-
+        
         if let response = mockResponses[requestKey] as? T.ResponseType {
             completion(.success(response))
         } else if let error = mockResponses[requestKey] as? NetworkErrors {
@@ -22,13 +24,22 @@ class MockNetworkManager: NetworkManagerProtocol {
             completion(.failure(.invalidResponse))
         }
     }
-
-
+    
+    
+    func downloadImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
+        if let mockImage = mockImage {
+            completion(mockImage)
+        } else {
+            completion(nil)
+        }
+    }
+    
+    
     func setMockResponse<T: BaseRequest>(for requestType: T.Type, response: T.ResponseType) {
         let requestKey = String(describing: T.self)
         mockResponses[requestKey] = response
     }
-
+    
     
     func setMockError<T: BaseRequest>(for requestType: T.Type, error: NetworkErrors) {
         let requestKey = String(describing: T.self)
